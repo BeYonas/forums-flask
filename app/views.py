45c1ab1c180058bf60878b9233b25c56a5dcc34c
Yonas-Be <36481484+BeYonas@users.noreply.github.com>
2018-03-17 20:13:44 +0100

    Python workshop Lesson 37:"CRUD with views"
    Mission required:
    • Add topic_delete function allowing the ability to delete posts from the index page.
    • Add topic_show function allowing the ability to preview the content and title of the post .
    • Add topic_edit function allowing the ability to delete the post.
    
    Lesson 37 page : macdiscussions.udacity.com/t/37-crud-with-views/124179

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app, post_store, models
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html", posts=post_store.get_all())
+    return render_template("index.html", posts=enumerate(post_store.get_all()))
 
 
 @app.route("/topic/add", methods=["GET", "POST"])
@@ -14,6 +14,32 @@ def topic_add():
         new_post = models.Post(request.form["title"], request.form["content"])
         post_store.add(new_post)
         return redirect(url_for("home"))
-
     else:
         return render_template("topic_add.html")
+
+
+@app.route("/topic/edit/<int:post_id>", methods=["GET", "POST"])
+def topic_edit(post_id):
+    post_to_edit = post_store.get_by_id(post_id)
+    if request.method == "POST":
+        post_to_edit.title = request.form["title"]
+        post_to_edit.content = request.form["content"]
+        return redirect(url_for("home"))
+    else:
+        title = post_to_edit.title
+        content = post_to_edit.content
+        return render_template("topic_add.html", title=title, content=content, post_id=post_id)
+
+
+@app.route("/topic/show/<int:post_id>")
+def topic_show(post_id):
+    post_to_show = post_store.get_by_id(post_id)
+    title = post_to_show.title
+    content = post_to_show.content
+    return render_template("topic_show.html", title=title, content=content)
+
+
+@app.route("/topic/delete/<int:post_id>")
+def topic_delete(post_id):
+    post_store.delete(post_id)
+    return redirect(url_for("home"))
